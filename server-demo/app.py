28ebdf14b40bc4959a75757ab062d6f81b82d9f0
Shushu Fang <sfang22@mit.edu>
2019-09-14 19:16:14 -0400

    flask framework done

---
+++ server-demo/app.py
@@ -0,0 +1,29 @@
+import os
+from flask import Flask, request, jsonify,render_template
+
+#Initialize Flask
+app = Flask(__name__)
+
+#Initialize Firebase DB
+import pyrebase
+
+config = {
+  "apiKey": "AIzaSyCA64ir3rDXaer9gcExR2k2sMUuUQA_5PU",
+  "authDomain": "hack-mit-2e096.firebaseapp.com",
+  "databaseURL": "https://hack-mit-2e096.firebaseio.com",
+  "storageBucket": "hack-mit-2e096.appspot.com",
+  "serviceAccount": "hack-mit-2e096-firebase-adminsdk-u0606-f54e8a8410.json"
+}
+firebase = pyrebase.initialize_app(config)
+
+@app.route('/')
+def demo():
+    return render_template('index.html')
+
+@app.route('/list', methods=['GET'])
+def read():
+    return "hello"
+
+
+if __name__ == "__main__":
+    app.run(host= '0.0.0.0')
